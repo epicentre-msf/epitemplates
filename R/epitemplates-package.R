f50c41f5e6505b374a4349011cba8a21fc3e6ed4
Yves Amevoin <amevoina@yahoo.fr>
2024-07-04 03:57:20 +0200

    added function for downloading report/book

---
+++ R/epitemplates-package.R
@@ -0,0 +1,10 @@
+#' Generate templates for EpiDS documents
+#'
+#' A collection of Rmarkdown templates and R functions to use quarto
+#' templates within Rstudio and VSCodes.
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
